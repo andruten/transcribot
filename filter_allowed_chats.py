2c29328e3723b811067cb1ff0e95e732b04670e4
andruten <andruten@users.noreply.github.com>
2023-04-30 16:37:32 +0200

    Improved chat filtering

--- filter_allowed_chats.py
+++ filter_allowed_chats.py
@@ -1,8 +1,8 @@
+import logging
 from typing import List
 
 from telegram import Message
 from telegram.ext.filters import MessageFilter
-import logging
 
 logger = logging.getLogger(__name__)
 
@@ -13,17 +13,25 @@ class FilterAllowedChats(MessageFilter):
         super().__init__()
         self.allowed_chat_ids = allowed_chat_ids
 
+    def _effective_message(self, message: Message):
+        return message if message.reply_to_message is None else message.reply_to_message
+
+    def _is_voice(self, message: Message) -> bool:
+        effective_message = self._effective_message(message)
+        return bool(effective_message.voice) or \
+            bool(effective_message.audio) or \
+            bool(effective_message.video) or \
+            bool(effective_message.document)
+
     def filter(self, message: Message) -> bool:
         chat_id = str(message.chat.id)
         is_allowed_user = chat_id in self.allowed_chat_ids
         if not is_allowed_user:
             logger.info(f'chat_id="{chat_id}" is not allowed')
-            return False
 
-        is_voice = bool(message.voice) or bool(message.audio) or bool(message.video) or bool(message.document)
+        is_voice = self._is_voice(message)
         if not is_voice:
             logger.debug(f'chat_id={chat_id}: ignoring a message because it is not a voice or audio or video or '
                          f'document message')
-            return False
 
         return is_voice and is_allowed_user
