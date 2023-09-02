66396c77e5cb04919736ff08acc23948377270d3
andruten <andruten@users.noreply.github.com>
2023-09-02 11:55:14 +0200

    Revert "Feat replace whisper model (#15)"
    This reverts commit c42c95c68abaa9353947e45c10db83cdb46f06b5.

--- message_transcriber.py
+++ message_transcriber.py
@@ -10,10 +10,8 @@ class AudioMessageTranscriber:
     async def transcribe(context: CallbackContext, audio) -> str:
         async with TelegramAudioManager(context, audio) as mp3_audio_path:
             try:
-                segments, transcription_info = (
-                    audio_transcriber.transcribe_audio(mp3_audio_path)
-                )
-                text = audio_transcriber.get_as_markdown(segments, transcription_info)
+                result, processing_time = audio_transcriber.transcribe_audio(mp3_audio_path)
+                text = audio_transcriber.get_as_markdown(result, processing_time)
             except Exception as e:
                 text = f'Error converting video to audio. Exception={e}'
         return text
--- transcriber.py
+++ transcriber.py
@@ -1,31 +1,32 @@
 import logging
 import os
-from typing import Dict, Iterator
+import time
+from typing import Dict
 
-import faster_whisper
+import whisper
 
 logger = logging.getLogger(__name__)
 
 whisper_model = os.environ.get('WHISPER_MODEL', default='tiny')
 device = os.environ.get('WHISPER_DEVICE', default='cpu')
 
-whisper_model = model = faster_whisper.WhisperModel(
-    whisper_model,
-    device=device,
-    compute_type='float32',
-)
+whisper_model = whisper.load_model(whisper_model, device=device)
 
 
 class AudioTranscriber:
 
     @property
-    def model(self) -> faster_whisper.WhisperModel:
+    def model(self) -> whisper.Whisper:
         return whisper_model
 
     def transcribe_audio(self, mp3_audio_path: str) -> [Dict, float]:
-        segments, transcription_info = self.model.transcribe(mp3_audio_path)
-        logger.info('Audio processed')
-        return segments, transcription_info
+        start_time = time.time()
+        audio = whisper.load_audio(mp3_audio_path)
+        result = self.model.transcribe(audio, fp16=False)
+        final_time = time.time()
+        processing_time = final_time - start_time
+        logger.info(f'Audio processed in {processing_time}')
+        return result, processing_time
 
     def escape_markdown_chars(self, text: str) -> str:
         escaping_chars = ['_', '*', '[', ']', '(', ')', '~', '>', '+', '-', '=', '|', '{', '}', '.', '!']
@@ -34,12 +35,12 @@ class AudioTranscriber:
             temporal = temporal.replace(char, f'\\{char}')
         return temporal.replace(r'\. ', '.\n\n').replace(r'\.', '.')
 
-    def get_as_markdown(self, segments: Iterator, transcription_info) -> str:
-        transcription = ''
-        for segment in segments:
-            transcription += f'{segment.text}'
+    def get_as_markdown(self, text: Dict, processing_time) -> str:
+        transcription = text['text'].removeprefix(' ')
+        language_ = text['language']
         markdown_message = f'''\
-Detected language: {transcription_info.language}
+Detected language: {language_}
+Processing time: {int(processing_time)}s
 Transcription:
 ```
 {transcription}
